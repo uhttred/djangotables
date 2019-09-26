5f3dcb4335bd9288aa131dc36a27d25fd3afbf5b
Ageu Matheus <ageumatheus1@gmail.com>
2019-09-26 12:22:17 +0100

    setup and history prepared

---
+++ HISTORY.md
@@ -0,0 +1,12 @@
+History
+=======
+
+Pending
+-------
+
+.. Insert new release notes below this line
+
+0.0.1 (26-09-2019)
+-----------------
+
+* Initial release
---
+++ setup.py
@@ -0,0 +1,53 @@
+import os
+import re
+
+from setuptools import setup
+
+
+def get_version(filename):
+    with open(filename, "r") as fp:
+        contents = fp.read()
+    return re.search(r"__version__ = ['\"]([^'\"]+)['\"]", contents).group(1)
+
+version = get_version(os.path.join("djangotables", "__init__.py"))
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.md", "r") as history_file:
+    history = history_file.read()
+
+setup(
+    name="djangotables",
+    version=version,
+    description=(
+        "djangotables is a simple library for generating html "
+        "tables with Django Framework using model data like django forms."
+    ),
+    long_description=readme + "\n\n" + history,
+    author="Ageu Matheus",
+    author_email="ageumatheus1@gmail.com",
+    maintainer="Ageu Matheus",
+    maintainer_email="ageumatheus1@gmail.com",
+    url="",
+    packages=["djangotables"],
+    license="MIT License",
+    keywords=["django", "tables", "djangotables"],
+    install_requires=["Django>=2.2"],
+    python_requires=">=3.5",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
